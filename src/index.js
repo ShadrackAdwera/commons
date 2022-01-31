de0f52cda3df298214ddcb7a7b8fa98bfcf7f7ab
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-31 18:14:17 +0300

    generate ts config file

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const HttpError = require('./error/httpError');
-const checkAuth = require('./middlewares/checkAuth');
-const Listener = require('./events/base-listener');
-const Publisher = require('./events/base-publisher');
-
-exports.HttpError = HttpError;
-exports.checkAuth = checkAuth;
-exports.Listener = Listener;
-exports.Publisher = Publisher;
