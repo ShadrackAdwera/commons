28c8c349c28523d737f958b2a998cc0a246e4191
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-03 13:35:56 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export * from './error/HttpError';
 export * from './middlewares/auth-middleware';
 
+//nats
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/subjects';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
+
