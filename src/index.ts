8214b73e06a45e1af63fe89ee0c742896e47df61
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-08 13:23:43 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export * from './events/ticket-updated-event';
 export * from './events/order-created-event';
 export * from './events/order-cancelled-event';
 export * from './events/expiration-complete-event';
+export * from './events/payment-created-event';
 
 //enums
 export * from './events/types/order-status';
