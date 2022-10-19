d2bd11b4a5cbe65d68d6bc0c639c698d5b8cf229
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-19 16:10:22 +0300

    updates

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,12 +1,13 @@
-import { Request, Response, NextFunction } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
-import { HttpError } from "../error/HttpError";
+import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { HttpError } from '../error/HttpError';
 
 interface JwtPayloadProps extends JwtPayload {
   userId: string;
   email: string;
+  tenantId: string;
 }
-
+// so decode jwt using userid, email, tenantId
 // to modify an existing interface . . .
 declare global {
   namespace Express {
@@ -17,22 +18,26 @@ declare global {
 }
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     return next();
   }
 
   let decodedToken: JwtPayloadProps;
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
-    return next(new HttpError("Auth failed", 401));
+    return next(new HttpError('Auth failed', 401));
   }
 
   try {
     decodedToken = <JwtPayloadProps>jwt.verify(token, process.env.JWT_KEY!);
-    req.user = { userId: decodedToken.id, email: decodedToken.email };
+    req.user = {
+      userId: decodedToken.userId,
+      email: decodedToken.email,
+      tenantId: decodedToken.tenantId,
+    };
     next();
   } catch (error) {
-    return next(new HttpError("Auth failed", 401));
+    return next(new HttpError('Auth failed', 401));
   }
 };
